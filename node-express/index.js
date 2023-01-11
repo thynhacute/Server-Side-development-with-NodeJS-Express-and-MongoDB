22b9a2b0ac551a708343f8bb7d1c94d5ffc38b9a
Hoàng Nhã Thy <77708167+thynhacute@users.noreply.github.com>
2023-01-11 14:32:24 +0700

    express router

--- node-express/index.js
+++ node-express/index.js
@@ -3,16 +3,23 @@ const express = require("express"),
 
 const hostname = "localhost";
 const port = 5000;
-
+const morgan = require("morgan");
 const app = express();
 
-app.use((req, res, next) => {
-  console.log(req.headers);
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end("<html><body><h1>This is an Express Server</h1></body></html>");
-});
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.json());
+
+const nationRouter = require("./routes/nationRouter");
+
+app.use("/nations", nationRouter);
+
+const playerRouter = require('./routes/playerRouter');
+app.use('/players',playerRouter);
+
+
+app.use(morgan("dev"));
+app.use(express.static(__dirname + "/public"));
 const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
---
+++ node-express/routes/nationRouter.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const nationRouter = express.Router();
+
+nationRouter.use(bodyParser.json());
+
+nationRouter.route("/").all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    next();
+  })
+  .get((req, res, next) => {
+    res.end("Will send all the nations to you!");
+  })
+  .post((req, res, next) => {
+    res.end(
+      "Will add the nation: " + req.body.name + " with details: " + req.body.description
+    );
+  })
+  .put((req, res, next) => {
+    res.statusCode = 403;
+    res.end("PUT operation not supported on /nations");
+  })
+  .delete((req, res, next) => {
+    res.end("Deleting all nations");
+  });
+  nationRouter.route('/:nationId')
+  .get((req, res, next) => {
+    res.end("Will send all the nations to you!");
+  })
+  .post((req, res, next) => {
+    res.end(
+      "Will add the nation: " + req.body.name + " with details: " + req.body.description
+    );
+  })
+  .put((req, res, next) => {
+    res.statusCode = 403;
+    res.end("PUT operation not supported on /nations");
+  })
+  .delete((req, res, next) => {
+    res.end("Deleting all nations");
+  });
+module.exports = nationRouter;
